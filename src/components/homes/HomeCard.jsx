8f111a06baf0e6c3750fd6e1b20d5dfc47434455
pavanc1605 <80194959+pavanc1605@users.noreply.github.com>
2024-02-18 17:36:22 +0530

    Add files via upload

---
+++ src/components/homes/HomeCard.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const HomeCard = ({ item: { id, cover, name, rating, time, desc, starring, genres, tags, video } }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const toggleVideoPlayback = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  const closeVideo = () => {
+    setIsPlaying(false);
+  };
+
+  return (
+    <>
+      <div className='box'>
+        <div className='coverImage'>
+          <img src={cover} alt='' />
+        </div>
+        <div className='content flex'>
+          <div className='details row'>
+            <h1>{name}</h1>
+            <div className='rating flex'>
+              <div className='rate'>
+                <i className='fas fa-star'></i>
+                <i className='fa fa-star'></i>
+                <i className='fa fa-star'></i>
+                <i className='fa fa-star'></i>
+                <i className='fa fa-star-half'></i>
+              </div>
+              <label>{rating}(Imdb)</label>
+              <span>GP</span>
+              <label>{time}</label>
+            </div>
+            <p>{desc}</p>
+            <div className='cast'>
+              <h4>
+                <span>Starring </span>
+                {starring}
+              </h4>
+              <h4>
+                <span>Genres </span>
+                {genres}
+              </h4>
+              <h4>
+                <span>Tags </span>
+                {tags}
+              </h4>
+            </div>
+            <button className='primary-btn' onClick={toggleVideoPlayback}>
+              <i className='fas fa-play'></i> PLAY NOW
+            </button>
+          </div>
+          <div className='palyButton row'>
+            {isPlaying ? (
+              <div className="video-player-container">
+                <video controls>
+                  <source src={video} type="video/mp4" />
+                </video>
+                <button className="close-button" onClick={closeVideo}>Close</button>
+              </div>
+            ) : (
+              <Link to={`/singlepage/${id}`}>
+                {/* <button>
+                  <div className='img'>
+                    <img src='./images/play-button.png' alt='' />
+                    <img src='./images/play.png' className='change' />
+                  </div>
+                  WATCH TRAILER
+                </button> */}
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomeCard;
